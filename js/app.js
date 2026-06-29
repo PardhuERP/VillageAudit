94fad02db86db69c5a26fe2acfabc309736277e5
PardhuERP <sunny.pardhu@gmail.com>
2026-06-29 13:19:19 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+const API =
+"https://script.google.com/macros/s/AKfycby2PHdv3C9YFmZZ7akKazW5aqPX337oxcBwgVD65NpGC_5bUMSnLttaCmqSds0d6Yv_Eg/exec";
 
+async function getConfig() {
+
+    const res = await fetch(API + "?action=getConfig");
+
+    const json = await res.json();
+
+    return json.data;
+
+}
