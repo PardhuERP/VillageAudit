6f9ebaac7cec9c311bf7df32f88963b104eeecc3
PardhuERP <sunny.pardhu@gmail.com>
2026-07-05 00:00:44 +0530

    Update report.js

--- js/report.js
+++ js/report.js
@@ -1,23 +1,52 @@
-const API = "https://script.google.com/macros/s/AKfycby2PHdv3C9YFmZZ7akKazW5aqPX337oxcBwgVD65NpGC_5bUMSnLttaCmqSds0d6Yv_Eg/exec";
+const API="https://script.google.com/macros/s/AKfycby2PHdv3C9YFmZZ7akKazW5aqPX337oxcBwgVD65NpGC_5bUMSnLttaCmqSds0d6Yv_Eg/exec";
 
-async function loadStates() {
 
-    const res = await fetch(API + "?action=getStates");
-    const json = await res.json();
+window.onload=function(){
 
-    const state = document.getElementById("state");
+loadStates();
 
-    state.innerHTML = "<option value=''>Select State</option>";
+};
 
-    json.data.forEach(s => {
+async function loadStates(){
 
-        state.innerHTML +=
-        `<option value="${s.code}">${s.name}</option>`;
+const res=await fetch(API+"?action=getStates");
 
-    });
+const json=await res.json();
+
+const state=document.getElementById("state");
+
+state.innerHTML="<option value=''>Select State</option>";
+
+json.data.forEach(s=>{
+
+state.innerHTML+=`<option value="${s.code}">${s.name}</option>`;
+
+});
 
 }
 
-window.onload = function () {
-    loadStates();
-};
+document.getElementById("state").addEventListener("change",function(){
+
+loadDistricts(this.value);
+
+});
+
+async function loadDistricts(stateCode){
+
+const district=document.getElementById("district");
+
+district.innerHTML="<option>Loading...</option>";
+
+const res=await fetch(API+"?action=getDistricts&state="+stateCode);
+
+const json=await res.json();
+
+district.innerHTML="<option value=''>Select District</option>";
+
+json.data.forEach(d=>{
+
+district.innerHTML+=`<option value="${d.code}">${d.name}</option>`;
+
+});
+
+}
