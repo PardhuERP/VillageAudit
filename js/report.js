a7d63a54f0fe529a7d19d3a817091709d46b0e79
PardhuERP <sunny.pardhu@gmail.com>
2026-07-06 19:08:25 +0530

    Initialize reportTemplates array in report.js

--- js/report.js
+++ js/report.js
@@ -1,5 +1,5 @@
 const API = "https://script.google.com/macros/s/AKfycby2PHdv3C9YFmZZ7akKazW5aqPX337oxcBwgVD65NpGC_5bUMSnLttaCmqSds0d6Yv_Eg/exec";
-
+let reportTemplates = [];
 // =======================================
 // ELEMENTS
 // =======================================
